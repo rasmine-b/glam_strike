1b7ab95608867b0f33af05dcfd1432d7d476cf5d
rasmine <blasrasmine@gmail.com>
2025-06-23 15:09:44 +0800

    add battle log display

--- battle_ui.py
+++ battle_ui.py
@@ -4,4 +4,10 @@ class BattleUi:
     def update_health(player_health_label, enemy_health_label, player, enemy):
         player_health_label.config(text=f"HP: {player.health}/{player.max_health}")
         enemy_health_label.config(text=f"HP: {enemy.health}/{enemy.max_health}")
+
+    def log_message(log_widget, message, tag=None):
+        log_widget.config(state='normal')
+        log_widget.insert('end', message + '\n', tag)
+        log_widget.see('end')
+        log_widget.config(state='disabled')
         
