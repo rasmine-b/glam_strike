55444270a7870604805ae7b01dcd38536e949b87
rasmine <blasrasmine@gmail.com>
2025-06-23 15:37:07 +0800

    Init GlamStrike main UI

--- game.py
+++ game.py
@@ -12,3 +12,13 @@ from kazuya import Kazuya
 from lee import Lee
 
 from battle_ui import BatteleUi
+
+class GlamStrike:
+    def __init__(self, root):
+        self.root.title("Glam Strike: Fierce & Flawless")
+        self.root.attributes('-fullscreen', True)
+
+        self.ui = BattleUi()
+
+        self.bg_canvas = tk.Canvas(root, bg="#ffe6f0", highlightthickness=0)
+        self.bg_canvas.pack(fill="both", expand=True)
