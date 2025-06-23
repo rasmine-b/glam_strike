d36c690e78d067f220134a33da3b96414f04b18b
rasmine <blasrasmine@gmail.com>
2025-06-23 15:54:31 +0800

    add opponent selection screen

--- game.py
+++ game.py
@@ -60,9 +60,28 @@ class GlamStrike:
             tk.Button(self.bg_canvas, text=name, width=20, height=2,
                       font=('Comic Sans MS', 14, 'bold'), bg="#cceeff", fg="#004466",
                       activebackground="#b3e0ff",
-                      command=lambda n=name: self.select_player(n)).place(relx=0.7, rely=0.25 + i * 0.1, anchor="center")
+                      command=lambda name=name: self.select_player(name)).place(relx=0.7, rely=0.25 + i * 0.1, anchor="center")
         
     def select_player(self, name):
         self.selected_player = self.characters[name]()
         self.choose_opponent_screen()
+
+    def choose_opponent_screen(self):
+        self.clear_window()
+
+        tk.Label(self.bg_canvas, text="🤜 Choose Your Opponent or Randomize 🤛",
+                 font=('Comic Sans MS', 24, 'bold'), fg="#3366cc", bg="#ffe6f0").place(relx=0.5, rely=0.1, anchor="center")
         
+        other_names = [name for name in self.characters if self.character[n] != type(self.selecter_player)]
+
+        for i, name in enumerate(other_names):
+            tk.Button(self.bg_canvas, text=name, width=20, height=2,
+                      font=('Comic Sans MS', 14, 'bold'),
+                      bg="#e6f2ff" if name in ["Jin", "Hwoarang", "Kazuya", "Lee"] else "#ffe6f2",
+                      fg="#003366" if name in ["Jin", "Hwoarang", "Kazuya", "Lee"] else "#99004d",
+                      command=lambda name=name: self.start_battle(name)).place(relx=0.5, rely=0.25 + i * 0.1, anchor="center")
+            
+        tk.Button(self.bg_canvas, text="🎲 Random Opponent", width=22, height=2,
+                font=('Comic Sans MS', 14, 'bold'),
+                bg="#e0ccff", fg="#330066",
+                command=self.random_opponent).place(relx=0.5, rely=0.8, anchor="center")
