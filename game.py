9f1ec5b136f75175842d3f3ab41818bdefd11b13
rasmine <blasrasmine@gmail.com>
2025-06-23 15:33:16 +0800

    import character and UI modules

---
+++ game.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import messagebox
+import random
+
+from chloe import Chloe
+from lili import Lili
+from asuka import Asuka
+from xiaoyu import Xiaoyu
+from jin import Jin
+from hwoarang import Hwoarang
+from kazuya import Kazuya
+from lee import Lee
+
+from battle_ui import BatteleUi
